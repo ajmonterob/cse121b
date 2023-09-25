46ba0a8f4cb2940e8809d95eedec43ecfc0bb8f9
Andres Montero <andres.josue.montero@gmail.com>
2023-09-25 01:16:46 -0400

    newchange

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -6,40 +6,34 @@
 /* Step 2 - Variables */
 let fullName = 'Andres Montero';
 const currentYear = new Date().getFullYear();
-const profilePicture = '/images/me.png';
-
+const profilePicture = 'images/me.png';
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-const imageElement = document.getElementById('img');
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
+
+/* Step 5 - Array */
+const favoriteFoods = ["Pizza", "Sushi", "Chocolate", "Burgers", "Ice Cream"];
+foodElement.innerHTML = favoriteFoods.join(", ") + "<br>";
+
+const anotherFavoriteFood = "Pasta";
+favoriteFoods.push(anotherFavoriteFood);
+foodElement.innerHTML += favoriteFoods.join(", ") + "<br>";
+
+favoriteFoods.shift();
+foodElement.innerHTML += favoriteFoods.join(", ") + "<br>";
+
+favoriteFoods.pop();
+foodElement.innerHTML += favoriteFoods.join(", ");
+
+
+const imageElement = document.getElementById('img');
 imageElement.setAttribute('src', profilePicture);
 
 const altText = `Profile image of ${fullName}`;
 imageElement.setAttribute('alt', altText);
-
-
-/* Step 5 - Array */
-const favoriteFoods = ["Pizza", "Sushi", "Chocolate", "Burgers", "Ice Cream"];
-
-foodElement.textContent = favoriteFoods.join(", ");
-
-const anotherFavoriteFood = "Pasta";
-favoriteFoods.push(anotherFavoriteFood);
-
-
-foodElement.innerHTML += `<br>${favoriteFoods}`;
-
-favoriteFoods.shift();
-
-
-foodElement.innerHTML += `<br>${favoriteFoods.join("<br>")}`;
-
-favoriteFoods.pop();
-
-foodElement.innerHTML += `<br>${favoriteFoods.join("<br>")}`;
