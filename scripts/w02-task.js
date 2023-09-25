1a994182bb5471e00ed91baad0cdc12bbeff85f4
Andres Montero <andres.josue.montero@gmail.com>
2023-09-25 02:22:46 -0400

    last

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -6,16 +6,23 @@
 /* Step 2 - Variables */
 let fullName = 'Andres Montero';
 const currentYear = new Date().getFullYear();
-const profilePicture = 'images/me.png';
+
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
+
+const profilePicture = "images/me.png";
+imageElement.setAttribute('src', profilePicture);
+
+const altText = `Profile image of ${fullName}`;
+imageElement.setAttribute('alt', altText);
 
 /* Step 5 - Array */
 const favoriteFoods = ["Pizza", "Sushi", "Chocolate", "Burgers", "Ice Cream"];
@@ -32,8 +39,6 @@ favoriteFoods.pop();
 foodElement.innerHTML += favoriteFoods.join(", ");
 
 
-const imageElement = document.getElementById('img');
-imageElement.setAttribute('src', profilePicture);
 
-const altText = `Profile image of ${fullName}`;
-imageElement.setAttribute('alt', altText);
+/* console.log(`src: ${imageElement.getAttribute('src')}`);
+console.log(`src: ${imageElement.getAttribute('alt')}`);*/
