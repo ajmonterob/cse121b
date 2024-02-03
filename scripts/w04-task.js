1625f43e99ea93284f2c6636b86d65bad80b5818
Andres Montero <andres.josue.montero@gmail.com>
2024-02-03 13:43:26 -0500

    Week4 Adding code version1

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -1,28 +1,61 @@
 /* LESSON 3 - Programming Tasks */
 
 /* Profile Object  */
-
-
+let myProfile = {
+    name: "Andres Montero",
+    photo: "images/me.png",
+    favoriteFoods: [
+      'Rice',
+      'Tikka Masala',
+      'Prioshki',
+      'Shrimp',
+      'Banana Cream Pie'
+    ],
+    hobbies: [
+      'Swimming',
+      'Reading',
+      'Coding',
+      'Cooking',
+      'Traveling'
+    ],
+    placesLived: []
+};
 
 
 /* Populate Profile Object with placesLive objects */
-
-
-
+myProfile.placesLived.push({ place: 'Los Angeles, CA', length: '2 years' }).push({ place: 'New York, NY', length: '3 years' });
 
 /* DOM Manipulation - Output */
 
 /* Name */
+document.querySelector('#name').textContent = myProfile.name;
 
 /* Photo with attributes */
-
+document.querySelector('#photo').src = myProfile.photo;
 
 /* Favorite Foods List*/
+myProfile.favoriteFoods.forEach(food => {
+  let li = document.createElement('li');
+  li.textContent = food;
+  document.querySelector('#favorite-foods').appendChild(li);
+});
 
 
 /* Hobbies List */
-
+myProfile.hobbies.forEach(hobbie => {
+    let li = document.createElement('li');
+    li.textContent = hobbie;
+    document.querySelector('#hobbies').appendChild(li);
+  });
 
 /* Places Lived DataList */
+myProfile.placesLived.forEach(place => {
+    let dt = document.createElement('dt');
+    dt.textContent = place.place;
+    document.querySelector('#places-lived').appendChild(dt);
 
+    let dd = document.createElement('dd');
+    dd.textContent = place.length;
+    document.querySelector('#places-lived').appendChild(dd);
+});
 
